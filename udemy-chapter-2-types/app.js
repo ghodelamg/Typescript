b771960f5f3262326ae41e8f5835a5fe6eec4bec
Mukesh <mukesh.easternts@gmail.com>
2021-05-06 08:35:37 +0530

    Types Part 1

---
+++ udemy-chapter-2-types/app.js
@@ -0,0 +1,6 @@
+var myName = "Mukesh Ghodela";
+var age = 23;
+var hobbies = ['Reading', 'Travelling', 'Music'];
+var officeNumbers = [351, 588, 81];
+var mainOffice = [301, 'First Floor', 'Manager Office', true];
+var attendences = [true, false, true, true];
---
+++ udemy-chapter-2-types/app.ts
@@ -0,0 +1,6 @@
+let myName: string = "Mukesh Ghodela";
+let age: number = 23;
+let hobbies: string[] = ['Reading', 'Travelling', 'Music'];
+let officeNumbers: number[] = [351, 588, 81];
+let mainOffice: array = [301, 'First Floor', 'Manager Office', true];
+let attendences: boolean[] = [true, false, true, true];
