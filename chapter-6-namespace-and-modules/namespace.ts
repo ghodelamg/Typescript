1ff17edf0217d7ea13388e10347056542998063a
Mukesh <mukesh.easternts@gmail.com>
2021-05-20 08:42:23 +0530

    Namescapce

---
+++ chapter-6-namespace-and-modules/namespace.ts
@@ -0,0 +1,39 @@
+// The namespace is used for logical grouping of functionalities. 
+// A namespace can include interfaces, classes, functions and variables to support a single or a group of related functionalities.
+
+// A namespace can be created using the namespace keyword followed by the namespace name. All the interfaces, classes etc. 
+// can be defined in the curly brackets { }.
+
+namespace Validation {
+  export interface StringValidator {
+    isAcceptable(s: string): boolean;
+  }
+  const lettersRegexp = /^[A-Za-z]+$/;
+  const numberRegexp = /^[0-9]+$/;
+  export class LettersOnlyValidator implements StringValidator {
+    isAcceptable(s: string) {
+      return lettersRegexp.test(s);
+    }
+  }
+  export class ZipCodeValidator implements StringValidator {
+    isAcceptable(s: string) {
+      return s.length === 5 && numberRegexp.test(s);
+    }
+  }
+}
+// Some samples to try
+let strings = ["Hello", "98052", "101"];
+// Validators to use
+let validators: { [s: string]: Validation.StringValidator } = {};
+validators["ZIP code"] = new Validation.ZipCodeValidator();
+validators["Letters only"] = new Validation.LettersOnlyValidator();
+// Show whether each string passed each validator
+for (let s of strings) {
+  for (let name in validators) {
+    console.log(
+      `"${s}" - ${
+        validators[name].isAcceptable(s) ? "matches" : "does not match"
+      } ${name}`
+    );
+  }
+}
